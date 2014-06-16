89264d564ee2bdbac08b097b1af81328e496e7bb
Franck Mazzolo <franck.mazzolo@gmail.com>
2014-06-17 00:05:40 +0200

    Forgot in previous commit

--- functions.php
+++ functions.php
@@ -4,4 +4,30 @@
  * User: franckmazzolo
  * Date: 17/06/2014
  * Time: 00:00
- */ 
+ */
+// ref to questions and answers file
+const ANSWERS_FILE = "data/bonne_reponse.txt";
+const QUESTIONS_FILE = "data/questions.txt";
+include_once('FileReader.php');
+
+function getAnswers(){
+
+    $fileReader = new \utilities\FileReader();
+
+    //Get answers from ANSWERS_FILE
+    $answers = $fileReader->readFileToArray(ANSWERS_FILE);
+    return $answers;
+}
+
+function getQuestions(){
+
+    $fileReader = new \utilities\FileReader;
+
+    //Get questions from QUESTIONS_FILE
+    $questions = $fileReader->readFileToArray(QUESTIONS_FILE);
+    foreach($questions as &$question){
+        $question = str_getcsv($question,  $delimiter = ";" );
+    }
+
+    return $questions;
+}
